d837393ff6b32ec2a31e6c908bf71732f829849a
Olatoyan <golatoyan@gmail.com>
2023-04-27 02:25:04 +0100

    new commit

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const hoursCountdown = document.querySelector(".countdown__number--hours");
 const minuteCountdown = document.querySelector(".countdown__number--minutes");
 const secondsCountdown = document.querySelector(".countdown__number--seconds");
 const submitButton = document.querySelector(".button");
+const resetButton = document.querySelector(".reset");
 const inputField = document.querySelector(".input");
 const yearInput = document.getElementById("year");
 const monthInput = document.getElementById("month");
@@ -13,6 +14,7 @@ const yearError = document.querySelector(".year__error");
 const monthError = document.querySelector(".month__error");
 const dayError = document.querySelector(".day__error");
 const error = document.querySelector(".error");
+const inputs = document.querySelectorAll("input");
 
 dayInput.addEventListener("input", function () {
   dayInput.value = dayInput.value.replace(/[^0-9]/g, "");
@@ -28,7 +30,11 @@ yearInput.addEventListener("input", function () {
   yearInput.value = year.replace(/[^0-9]/g, "");
 });
 
+let updateCountdown;
+
 const getLaunchDate = function () {
+  clearInterval(updateCountdown);
+
   const now = new Date();
 
   // this is the day input
@@ -92,25 +98,25 @@ const getLaunchDate = function () {
     yearInput.style.border = "initial";
   }
 
-  const countdownDate = new Date(year, month - 1, day).getTime();
-  const present = new Date().getTime();
-  const timeRemaining = countdownDate - present;
-  if (countdownDate < present) {
-    error.style.display = "block";
-    return false;
-  } else {
-    error.style.display = "none";
-  }
+  updateCountdown = setInterval(() => {
+    const countdownDate = new Date(year, month - 1, day).getTime();
+    const present = new Date().getTime();
+    const timeRemaining = countdownDate - present;
+    if (countdownDate < present) {
+      error.style.display = "block";
+      return false;
+    } else {
+      error.style.display = "none";
+    }
 
-  const daysInMonth = new Date(year, month, 0).getDate();
-  if (day > daysInMonth) {
-    dayError.style.display = "block";
-    return false;
-  } else {
-    dayError.style.display = "none";
-  }
+    const daysInMonth = new Date(year, month, 0).getDate();
+    if (day > daysInMonth) {
+      dayError.style.display = "block";
+      return false;
+    } else {
+      dayError.style.display = "none";
+    }
 
-  const interval = setInterval(() => {
     const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
       (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -124,16 +130,66 @@ const getLaunchDate = function () {
     hoursCountdown.textContent = `${hours}`.padStart(2, 0);
     minuteCountdown.textContent = `${minutes}`.padStart(2, 0);
     secondsCountdown.textContent = `${seconds}`.padStart(2, 0);
-    return true;
   }, 1000);
+  // inputs.forEach((input) => {
+  //   input.disabled = true;
+  // });
+
+  // if (countdownDate < present) {
+  //   error.style.display = "block";
+  //   inputs.forEach((input) => {
+  //     input.disabled = false;
+  //   });
+  //   return false;
+  // } else {
+  //   error.style.display = "none";
+  // }
+
+  // if (countdownDate < present) {
+  //   error.style.display = "block";
+  //   resetCountdown();
+  //   return false;
+  // }
 };
+submitButton.addEventListener("click", getLaunchDate);
+
+const resetCountdown = function () {
+  clearInterval(updateCountdown);
+  // Reset all input fields to their default values
+  dayInput.value = "";
+  monthInput.value = "";
+  yearInput.value = "";
+
+  // Hide all error messages and reset borders
+  dayError.style.display = "none";
+  dayInput.style.border = "initial";
+  monthError.style.display = "none";
+  monthInput.style.border = "initial";
+  yearError.style.display = "none";
+  yearInput.style.border = "initial";
+  error.style.display = "none";
+
+  // Enable all input fields
+  inputs.forEach((input) => {
+    input.disabled = false;
+  });
+
+  // Reset countdown values
+  daysCountdown.textContent = "00";
+  hoursCountdown.textContent = "00";
+  minuteCountdown.textContent = "00";
+  secondsCountdown.textContent = "00";
+};
+
+resetButton.addEventListener("click", resetCountdown);
+
+// function go() {
+//   setInterval(getLaunchDate, 1000);
+// }
 
 // const countdown = setInterval(() => {
 //   getLaunchDate;
 // }, 1000);
-
-submitButton.addEventListener("click", getLaunchDate);
-
 // const now = new Date();
 
 // const countdownDate = new Date(now.getFullYear(), monthInput - 1, dayInput);
